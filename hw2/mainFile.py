e65b3f7ddeab4ec6816165667254e179f9b40de4
ousta15 <ousta15@ku.edu.tr>
2021-12-13 00:36:44 +0300

    finished

--- hw2/mainFile.py
+++ hw2/mainFile.py
@@ -6,13 +6,9 @@ indicator_x = {"SL.TLF.TOTL.FE.ZS": "female_labor_force"}
 indicators_x = {"SL.TLF.TOTL.FE.ZS": "female_labor_force",'SE.ENR.PRIM.FM.ZS': "School_enrollment_GPI"}
 indicator_y = {"SP.ADO.TFRT": "adolescent_fertility_rate"}
 
-
 X1 = wbdata.get_dataframe(indicator_x, country="TUR", convert_date=True)
 X2 = wbdata.get_dataframe(indicators_x, country="TUR", convert_date=True)
 y = wbdata.get_dataframe(indicator_y, country="TUR", convert_date=True)
-
-
-
 
 with open('hw2.csv', 'w') as f:
     linearReg(X1,y).to_csv(f)
