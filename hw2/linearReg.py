0b74069ade8ded0ea7ab1d1e0a913f7f0e8e1e27
ousta15 <ousta15@ku.edu.tr>
2021-12-13 00:08:31 +0300

    finished

--- hw2/linearReg.py
+++ hw2/linearReg.py
@@ -5,34 +5,46 @@ def linearReg(X,y):
 
     if len(X) == len(y):
 
+        variable_names = X.columns
         total = pd.concat([X, y], axis=1)
         total = total.dropna()
 
         if len(total)>0:
 
-            X = total.iloc[:,:-1].to_numpy()
-            y = total.iloc[:,-1:].to_numpy()
+            try:
 
-            X = np.hstack((np.ones(len(X)).reshape(len(X),1),X))
+                X = total.iloc[:,:-1].to_numpy()
+                y = total.iloc[:,-1:].to_numpy()
 
-            beta = np.matmul(np.matmul(np.linalg.inv(np.matmul(np.transpose(X),X)),np.transpose(X)),y)
-            yhat = np.matmul(X,beta)
-            beta = beta.reshape(1,-1)
-            e = y - yhat
-            e_var = np.matmul(np.transpose(e),e) / (len(y)-len(X[0])-1)
-            b_var = np.diag(e_var*np.linalg.inv(np.matmul(np.transpose(X),X)))
-            b_std_error = np.sqrt(b_var)
-            t_stat = np.abs(beta / b_std_error)
+                X = np.hstack((np.ones(len(X)).reshape(len(X),1),X))
 
-            results = (np.vstack((beta,b_std_error,t_stat)).transpose())
+                beta = np.matmul(np.matmul(np.linalg.inv(np.matmul(np.transpose(X),X)),np.transpose(X)),y)
+                yhat = np.matmul(X,beta)
+                beta = beta.reshape(1,-1)
+                e = y - yhat
+                e_var = np.matmul(np.transpose(e),e) / (len(y)-len(X[0])-1)
+                b_var = np.diag(e_var*np.linalg.inv(np.matmul(np.transpose(X),X)))
+                b_std_error = np.sqrt(b_var)
+                t_stat = np.abs(beta / b_std_error)
 
-            df_results = pd.DataFrame(results,columns=["coef","std_err","t_stat"]).round(2)
-            df_results["p_value"] = t.cdf(-abs(df_results["t_stat"]), len(X)-2).round(2)
-            df_results["[0.025"] = df_results["coef"] - 1.96 * df_results["std_err"]
-            df_results["0.975]"] = df_results["coef"] + 1.96 * df_results["std_err"]
+                results = (np.vstack((beta,b_std_error,t_stat)).transpose())
+
+                df_results = pd.DataFrame(results,columns=["coef","std_err","t_stat"]).round(2)
+                df_results["p_value"] = t.cdf(-abs(df_results["t_stat"]), len(X)-2).round(2)
+                df_results["[0.025"] = round(df_results["coef"] - 1.96 * df_results["std_err"],4)
+                df_results["0.975]"] = round(df_results["coef"] + 1.96 * df_results["std_err"],4)
+                df_results = df_results.rename(index={0:"const"})
+
+                for i in range(len(variable_names)):
+                    df_results = df_results.rename(index={i+1: variable_names[i]})
+
+            except:
+                raise TypeError("Data should includes only integer and/or float data type")
 
 
             return df_results
+
+
 
         else: return "All rows contain at least one NAN value."
 
--- hw2/test.py
+++ hw2/test.py
@@ -10,5 +10,8 @@ class LinearRegTest(unittest.TestCase):
     def test_nan(self):
         self.assertTrue("All rows contain at least one NAN value.", linearReg(X = {'x1': [3,np.nan], 'x2': [np.nan,6]}, y = {"y":[4,np.nan]}))
 
+    def test_type(self):
+        self.assertRaises(TypeError,linearReg(X = {'x1': [3,"asdf"], 'x2': [8,6]}, y = {"y":[4,"ghjk"]}))
+
 if __name__ == '__main__':
     unittest.main()
